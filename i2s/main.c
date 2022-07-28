03bc14df25d001166c5ef04ad42b5374ad6220f9
Victor Suarez Rovere <suarezvictor@gmail.com>
2022-07-27 22:08:15 -0300

    i2s continuous fifo writing test

--- i2s/main.c
+++ i2s/main.c
@@ -7,18 +7,30 @@
 #include "irq.h"
 #include "uart.h"
 
-void _putchar(char c) { uart_write(c); } //this is to make printf work
+static inline void i2s_enable(void) { i2s_tx_ev_enable_write(1); }
+static inline void i2s_enqueue_sample(uint32_t sample) { *(uint32_t*) I2S_TX_MEMADDR = sample; }
+static inline int i2s_tx_full(void) { return i2s_tx_tx_stat_full_read(); }
+static inline int i2s_tx_almostfull(void) { return i2s_tx_tx_stat_almostfull_read(); }
+static inline void i2s_tx_clear(void) { i2s_tx_tx_ctl_reset_write(1); }
+//static void i2s_clear_pending_irq(void) { i2s_tx_ev_pending_write(/*i2s_tx_ev_pending_read()*/0); }
 
-void audio_send()
+void audio_send(void)
 {
 	uint32_t sample = 0;
-	while(!i2s_tx_tx_stat_almostfull_read())
+	while(!i2s_tx_full())
 	{
-      printf("Samples in fifo: %d\n", i2s_tx_tx_stat_wrcount_read());
+	  unsigned count = i2s_tx_tx_stat_wrcount_read();
+	  if(count == 0)
+	  {
+	    static int frames = 0;
+	    ++frames;
+		printf("Frame %d, estimated time %d\n", frames, frames*(512/2)/i2s_tx_tx_conf_lrck_freq_read());
+	  }
 	  sample += 100;
 	  sample = sample | (sample << 16);
-	  *(uint32_t*) I2S_TX_MEMADDR = sample;
+	  i2s_enqueue_sample(sample);
 	}
+    i2s_tx_clear();
 }
 
 int main(int argc, char **argv) {
