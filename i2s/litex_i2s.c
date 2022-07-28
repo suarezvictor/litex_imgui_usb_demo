b09e713a761190963161ab8f41d71eef1ad75380
Victor Suarez Rovere <suarezvictor@gmail.com>
2022-07-28 02:04:16 -0300

    split i2s in header and implementation

---
+++ i2s/litex_i2s.c
@@ -0,0 +1,30 @@
+// This file is Copyright (c) 2022 Victor Suarez Rovere <suarezvictor@gmail.com>
+// License: BSD-2-Clause
+
+#include "litex_i2s.h"
+#include "irq.h"
+
+void i2s_tx_isr(void)
+{
+    if(i2s_audio_send_cb)
+		i2s_audio_send_cb(I2S_FIFO_DEPTH);
+	i2s_tx_ev_pending_write(i2s_tx_ev_pending_read()); //clears IRQ
+}
+
+volatile unsigned i2s_tx_samples_count = 0;
+
+void i2s_tx_start(void)
+{
+    i2s_tx_clear();
+    i2s_tx_enable();
+    i2s_tx_ev_enable_tx_ready_write(1);
+    irq_setmask(irq_getmask() | (1 << I2S_TX_INTERRUPT));
+}
+
+void i2s_tx_stop(void)
+{
+	i2s_tx_disable();
+	i2s_tx_ev_enable_tx_ready_write(0);
+	irq_setmask(irq_getmask() & ~(1 << I2S_TX_INTERRUPT));
+}
+
--- i2s/litex_i2s.h
+++ i2s/litex_i2s.h
@@ -6,6 +6,9 @@
 
 #include <generated/csr.h>
 
+void i2s_tx_start(void);
+void i2s_tx_stop(void);
+
 static inline void i2s_tx_enable(void) { i2s_tx_tx_ctl_enable_write(1); }
 static inline void i2s_tx_disable(void) { i2s_tx_tx_ctl_enable_write(0); }
 static inline void i2s_tx_enqueue_sample(uint32_t sample) { *(uint32_t*) I2S_TX_MEMADDR = sample; }
@@ -19,29 +22,6 @@ static inline unsigned i2s_tx_get_bits(void) { return i2s_tx_tx_conf_sample_widt
 extern volatile unsigned i2s_tx_samples_count;
 static inline unsigned i2s_tx_played_count(void) { return i2s_tx_samples_count; }
 
-volatile unsigned i2s_tx_samples_count = 0;
-void __attribute__((weak)) i2s_audio_send_cb(size_t count);
-
-static void i2s_tx_start(void)
-{
-    i2s_tx_clear();
-    i2s_tx_enable();
-    i2s_tx_ev_enable_tx_ready_write(1);
-    irq_setmask(irq_getmask() | (1 << I2S_TX_INTERRUPT));
-}
-
-static void i2s_tx_stop(void)
-{
-	i2s_tx_disable();
-	i2s_tx_ev_enable_tx_ready_write(0);
-	irq_setmask(irq_getmask() & ~(1 << I2S_TX_INTERRUPT));
-}
-
-void i2s_tx_isr(void)
-{
-    if(i2s_audio_send_cb)
-		i2s_audio_send_cb(I2S_FIFO_DEPTH);
-	i2s_tx_ev_pending_write(i2s_tx_ev_pending_read()); //clears IRQ
-}
+void __attribute__((weak)) i2s_audio_send_cb(unsigned count);
 
 #endif
